31645ce72eb7ec1b0e9802f683104eb1f58cca96
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-01-18 17:43:53 -0800

    Add start script

--- api/app.py
+++ api/app.py
@@ -9,17 +9,17 @@ from api.resources.media import media_bp
 from models.base import Base
 from nguylinc_python_utils.sqlalchemy import init_sqlite_db
 
-flask_app = APIFlask(__name__, title="Photos API", version="0.1.0", spec_path="/openapi.yaml", docs_ui="rapidoc")
-socketio = SocketIO(flask_app, cors_allowed_origins="*")
+app = APIFlask(__name__, title="Photos API", version="0.1.0", spec_path="/openapi.yaml", docs_ui="rapidoc")
+socketio = SocketIO(app, cors_allowed_origins="*")
 session = init_sqlite_db(Base)
 
-flask_app.config["SPEC_FORMAT"] = "yaml"
-flask_app.config["LOCAL_SPEC_PATH"] = "openapi.yaml"
-flask_app.config["SYNC_LOCAL_SPEC"] = True
-CORS(flask_app, supports_credentials=False, origins="*", allow_headers="*")
+app.config["SPEC_FORMAT"] = "yaml"
+app.config["LOCAL_SPEC_PATH"] = "openapi.yaml"
+app.config["SYNC_LOCAL_SPEC"] = True
+CORS(app, supports_credentials=False, origins="*", allow_headers="*")
 
-flask_app.register_blueprint(album_bp)
-flask_app.register_blueprint(media_bp)
+app.register_blueprint(album_bp)
+app.register_blueprint(media_bp)
 
 
 @socketio.on("connect")
@@ -27,12 +27,13 @@ def on_connect():
     print("Client connected!")
 
 
-@flask_app.teardown_appcontext
+@app.teardown_appcontext
 def shutdown_session(exception=None):
     if session:
         session.remove()
 
-@flask_app.before_request
+
+@app.before_request
 def add_fake_delay():
     # fake_delay = 100
     # fake_delay = 0.5
@@ -45,4 +46,4 @@ port = 34200
 BASE_URL = "http://127.0.0.1:" + str(port)
 
 if __name__ == "__main__":
-    flask_app.run(port=port, debug=True)
+    app.run(port=port, debug=True)
---
+++ scripts/start.sh
@@ -0,0 +1,1 @@
+(cd api && flask run --host=0.0.0.0 --port 34201)
